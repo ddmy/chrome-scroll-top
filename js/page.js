e2e399e62c60eb7062fa737dcb22eb823b18a3da
ddmy <841312998@qq.com>
2020-12-01 04:26:23 +0800

    按钮支持拖拽

--- js/page.js
+++ js/page.js
@@ -7,6 +7,12 @@ let btnSty = {
   opacity: '0.8',
   color: '#fff'
 }
+let clientX, clientY, offsetX, offsetY
+let bottom = 100
+let right = 50
+let btnWidth = 50
+let btnHeight = 50
+let startTime = 0
 chrome.storage.sync.get({'to-top': '[]', btnSty }, function(items) {
   let arr = JSON.parse(items['to-top'])
   btnSty = items.btnSty
@@ -23,11 +29,35 @@ window.addEventListener('load', () => {
   if (!creatTop) return false
   let div = document.createElement('div')
   div.innerHTML = btnSty.text
-  let css = `width:50px; height: 50px; display:none; background-color: ${btnSty.backgroundColor}; color: ${btnSty.color}; font-size: 12px; line-height: 50px; text-align: center; cursor: pointer; position: fixed; right: 20px; bottom: 100px; z-index: 99999; opacity: ${btnSty.opacity}; border-radius: ${parseInt(btnSty.borderRadius)}px; background-image: url(${btnSty.backgroundImage}); background-repeat: no-repeat; background-size: cover; background-position: center;`
+  let css = `width:${btnWidth}px; height: ${btnHeight}px; display:none; background-color: ${btnSty.backgroundColor}; color: ${btnSty.color}; font-size: 12px; line-height: 50px; text-align: center; cursor: pointer; position: fixed; right: ${right}px; bottom: ${bottom}px; z-index: 99999; opacity: ${btnSty.opacity}; border-radius: ${parseInt(btnSty.borderRadius)}px; background-image: url(${btnSty.backgroundImage}); background-repeat: no-repeat; background-size: cover; background-position: center; user-select: none;`
   div.style.cssText = css
-  div.addEventListener('click', () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' })
+  div.addEventListener('mousedown', (event) => {
+    clientX = event.clientX
+    clientY = event.clientY
+    offsetX = event.offsetX
+    offsetY = event.offsetY
+    startTime = Date.now()
+    div.addEventListener('mousemove', btnMouseMove, true)
+    div.addEventListener('mouseup', btnDragEnd)
+    div.addEventListener('mouseout', btnDragEnd)
   })
+  // 鼠标拖动
+  function btnMouseMove (event) {
+    let clientXMove = event.clientX
+    let clientYMove = event.clientY
+    div.style.bottom = clientY - clientYMove + bottom + 'px'
+    div.style.right = clientX - clientXMove + right + 'px'
+  }
+  // 鼠标拖动结束
+  function btnDragEnd (event) {
+    if (event.type === 'mouseup' && Date.now() - startTime < 300) {
+      window.scrollTo({ top: 0, behavior: 'smooth' })
+    }
+    startTime = 0
+    this.removeEventListener('mousemove', btnMouseMove, true)
+    bottom = parseInt(this.style.bottom)
+    right = parseInt(this.style.right)
+  }
   document.body.appendChild(div)
   // 窗口高度
   let clientHeight = (document.body.clientHeight<document.documentElement.clientHeight)?document.body.clientHeight:document.documentElement.clientHeight
